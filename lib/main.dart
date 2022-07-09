206013276b398875935ffb1ae56980e87b6d5064
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2022-07-09 15:20:47 +0700

    Padding basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
-));
-
+      home: Home(),
+    ));
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -16,20 +15,26 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        child: ElevatedButton.icon(
-          onPressed: () {},
-          icon: Icon(Icons.mail),
-          label: Text('Test'),
-        )
+      // body: Container(
+      //   padding: EdgeInsets.fromLTRB(10,20,30,40),
+      //   margin: EdgeInsets.all(25),
+      //   color: Colors.grey.shade900,
+      //   child: Text(
+      //     'hello',
+      //     style: TextStyle(
+      //         color: Colors.grey.shade50
+      //     ),
+      //   ),
+      // ),
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: Text('hello'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: ()  {},
+        onPressed: () {},
         backgroundColor: Colors.red[600],
         child: Text('Click'),
       ),
     );
   }
 }
-
-
